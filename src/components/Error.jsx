d8b6502e2c5636dd8bd20f7cb0f6d3c7833ef0ce
Atik Hasan <atikhasan801@gmail.com>
2021-09-18 00:49:36 +0600

    hello

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,6 +6,7 @@ const Error = () => {
       <div className=" text-center error ">
         <h2>Sorry! Nothing found in this url.</h2>
         <p>please with access valid url</p>
+        <h1>Click on Home button in Navbar</h1>
       </div>
     </>
   );
